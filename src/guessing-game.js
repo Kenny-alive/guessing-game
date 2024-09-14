7025ccba65ca8ed502a1b391252b2fab9626431d
Kenny-alive <ksvcsv@inbox.ru>
2024-09-14 15:07:46 +0300

    finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.res = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.res = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
